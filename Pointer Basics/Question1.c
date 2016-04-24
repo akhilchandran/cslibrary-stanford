714c89faad89619479c0c0a78b80907a7d30a2c9
akhilchandran.t.r <akhilchandran.t.r@gmail.com>
2016-04-24 19:15:16 +0530

    some changes

--- Pointer Basics/Question1.c
+++ Pointer Basics/Question1.c
@@ -9,7 +9,7 @@ int main()
 					sized memory as pointee of x */
 	*x = 42; /* Dereferance x to store 42 in its pointee
 			which we just have allocated */
-	/* *y = 13; whould CRASH since it don't have a pointee */
+	/* *y = 13;  whould CRASH with segmentation fault since it don't have a pointee */
 	y = x; /* Sets y to point x's pointee */
 	*y = 13; /* Dereferance y to store 13 its pointee which
 			also pointee of x */
